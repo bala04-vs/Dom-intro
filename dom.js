10025dc1901c7a1b0c2382068ea37bd0cb8f934c
bala04-vs <venkatposina29@gmail.com>
2025-02-08 13:14:46 +0530

    add

---
+++ dom.js
@@ -0,0 +1,27 @@
+// Dom:document object model
+// it acts as a API
+// DOM METHODS:
+
+// GETTING HTML ELEMENTS WE HAVE:
+// 1.getElementById();-----it will returns the single Element.
+// console.log(document.getElementById("h1"));
+
+// // 2.getElementByClassName();---- it will accsess the multiple elements 
+// // the elements should be stored as array and object
+// console.log(document.getElementsByClassName("p1"));
+
+//3.getElementByTagName()-->it will returns all matching elements as a collection
+//4.queryselector()--->with this we can get based on id/class/tagname it will returns only first matching element
+//5. queryselectorAll()-->id/class/tagname.it will returns all matched elements
+
+
+// innerHtml:we modify the js elements with using html items
+// innerText:we modify the only text of the html elements
+// innerTextContent:mofify the text o fhtml element
+
+
+
+
+
+
+
